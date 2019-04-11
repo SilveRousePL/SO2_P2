b68a7de82594ac7048bd82c1d2131aefcc95e8f4
SilveRousePL <linuxiarz1624pl@gmail.com>
2019-04-11 15:47:07 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/SO2_P2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SO2_P2.dir/main.cpp.o"
+  "SO2_P2.pdb"
+  "SO2_P2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SO2_P2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
